8291ecea532375f73a12bbe30f5c55ec3688fe45
Bogdan Nita <bogdan.nita512@gmail.com>
2024-10-31 00:06:39 +0200

    Refactor decryption setup and add type logging
    Update the decryption function to handle private key buffers correctly by converting the private key from base64 to a buffer before usage. Inline logging has been added to verify the types of buffers during runtime, aiding in debugging decryption issues and ensuring that the decryption outputs are as expected.

--- functions/decrypt.js
+++ functions/decrypt.js
@@ -1,21 +1,24 @@
+/* eslint-disable no-console */
 const NodeRSA = require('node-rsa');
 const rc4 = require('./arc4');
 
 const decrypt = (privateKey, envKey, data) => {
   // Convert envKey from base64 to buffer
-
-  // eslint-disable-next-line no-console
-  console.log('privateKey: ', privateKey);
-  
+  const privateKeyBuffer = Buffer.from(privateKey, 'base64');
   const encryptedBuffer = Buffer.from(envKey, 'base64');
 
   // Initialize NodeRSA instance with the private key and set encryption scheme
-  const keyRSA = new NodeRSA(privateKey, 'private', { encryptionScheme: 'pkcs1' });
-  keyRSA.setOptions({ environment: 'browser' }); // Use browser-compatible option
+  const keyRSA = new NodeRSA(privateKeyBuffer, 'private', {
+    encryptionScheme: 'pkcs1',
+    environment: 'browser',
+  });
 
   try {
+    console.log('encryptedBuffer type:', encryptedBuffer.constructor.name); // Should be Buffer
+
     // Decrypt the envKey to get the decrypted key for RC4
-    const decrypted = keyRSA.decrypt(encryptedBuffer, 'buffer'); // Specify 'buffer' output
+    const decrypted = keyRSA.decrypt(encryptedBuffer, 'buffer'); // Ensure output is a buffer
+    console.log('decrypted type:', decrypted.constructor.name); // Should output Buffer if decryption was successful
 
     // Use RC4 with the decrypted key to decode the data
     const cipher = rc4(decrypted);
