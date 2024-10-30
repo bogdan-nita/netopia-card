c657dd75918e8d677ea2948798e22cc08e8c17b3
Bogdan Nita <bogdan.nita512@gmail.com>
2024-10-30 12:03:50 +0200

    Enhance decryption error handling & update package
    Added error handling in the decryption process to catch and report issues, improving the robustness of error management. Updated package dependencies, including the addition of 'node-rsa', reflecting a minor version bump. Ensured compatibility for browser environments with encryption schemes, addressing potential issues with decryption formats.

--- functions/decrypt.js
+++ functions/decrypt.js
@@ -3,18 +3,27 @@ const rc4 = require('./arc4');
 
 const decrypt = (privateKey, envKey, data) => {
   // Convert envKey from base64 to buffer
+
+  // eslint-disable-next-line no-console
+  console.log('privateKey: ', privateKey);
+  
   const encryptedBuffer = Buffer.from(envKey, 'base64');
 
   // Initialize NodeRSA instance with the private key and set encryption scheme
   const keyRSA = new NodeRSA(privateKey, 'private', { encryptionScheme: 'pkcs1' });
   keyRSA.setOptions({ environment: 'browser' }); // Use browser-compatible option
 
-  // Decrypt the envKey to get the decrypted key for RC4
-  const decrypted = keyRSA.decrypt(encryptedBuffer);
+  try {
+    // Decrypt the envKey to get the decrypted key for RC4
+    const decrypted = keyRSA.decrypt(encryptedBuffer, 'buffer'); // Specify 'buffer' output
 
-  // Use RC4 with the decrypted key to decode the data
-  const cipher = rc4(decrypted);
-  return cipher.decode(Buffer.from(data, 'base64'), 'utf8');
+    // Use RC4 with the decrypted key to decode the data
+    const cipher = rc4(decrypted);
+    return cipher.decode(Buffer.from(data, 'base64'), 'utf8');
+  } catch (error) {
+    console.error('Error during decryption:', error.message);
+    throw new Error('Decryption failed: check key or encrypted data format.');
+  }
 };
 
 module.exports = decrypt;
